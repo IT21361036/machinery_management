e0f432d3519fe088263f6248ec0d7676506b70c4
IT21361036 <it21361036@my.sliit.lk>
2023-04-29 11:31:10 +0530

    find by id user

--- api/routes/users.js
+++ api/routes/users.js
@@ -14,7 +14,7 @@ router.put("/:id", verifyUser, updateUser);
 //DELETE
 router.delete("/:id", verifyUser, deleteUser);
 //GET
-router.get("/:id", verifyUser, getUser);
+router.get("/:id", /* verifyUser, */ getUser);
 //GET ALL
 router.get("/", /* verifyAdmin ,*/ getUsers);
 
