a58a614d02b85bc6f3434b9c78da8402f09b1604
IT21361036 <it21361036@my.sliit.lk>
2023-04-30 17:09:45 +0530

    fix multiple rendering

--- client/src/hooks/useGetToenConfig.js
+++ client/src/hooks/useGetToenConfig.js
@@ -1,15 +1,16 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 export const useGetTokenConfig = () => {
   const { user } = useContext(AuthContext);
   const token = user?.access_token;
 
-  const config = {
-    headers: {
-      authorization: token,
-    },
-  };
-
-  return config;
+  return useMemo(
+    () => ({
+      headers: {
+        authorization: token,
+      },
+    }),
+    []
+  );
 };
